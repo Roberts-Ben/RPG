32560f711c0877c63f14f48ac60dc41a1488ad37
Ben Roberts <roberts.b93@gmail.com>
2019-07-09 14:01:48 +0100

    Initial Commit
    Basic scene & bar fill

---
+++ Assets/Scripts/ATBBar.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ATBBar : MonoBehaviour
+{
+    public Image fillBar;
+
+    public float fillDuration;
+    private float fillAmount;
+
+    void Update()
+    {
+        if(fillAmount >= 1)
+        {
+            //Stop time and process turn
+        }
+
+        fillAmount += 1.0f / fillDuration * Time.deltaTime;
+        fillBar.fillAmount = fillAmount;
+    }
+}
